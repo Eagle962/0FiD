23ba62b530d56407bbdc81be702212d983748550
eagle962 <91y272@gmail.com>
2025-09-30 19:47:04 +0800

    fix gitignore

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ OFD - Overfitting Detector
 
 __version__ = "0.1.0"
 
-from .adapters import get_adapter, BaseAdapter, SklearnAdapter
+from ofd.adapters import get_adapter, BaseAdapter, SklearnAdapter
 
 __all__ = [
     'get_adapter',
